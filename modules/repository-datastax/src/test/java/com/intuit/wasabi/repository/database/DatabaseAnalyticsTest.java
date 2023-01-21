e9d305aba550faaa4e0a72cd116a8da90dfdbe10
Rafael CP <rafael_it@hotmail.com>
2023-01-21 15:43:36 -0300

    fix: update hamcrest version 1.3 to 2.2

--- modules/repository-datastax/src/test/java/com/intuit/wasabi/repository/database/DatabaseAnalyticsTest.java
+++ modules/repository-datastax/src/test/java/com/intuit/wasabi/repository/database/DatabaseAnalyticsTest.java
@@ -37,11 +37,9 @@ import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.atLeastOnce;
@@ -76,27 +74,24 @@ public class DatabaseAnalyticsTest {
         verify(flyway, atLeastOnce()).migrate();
     }
 
-    // @Test(expected = RepositoryException.class)
-    // public void getRollupRowsTest() {
-    // Experiment.ID experimentId = Experiment.ID.newInstance();
-    // String rollupDate = "TEST";
-    // Parameters parameters = mock(Parameters.class, RETURNS_DEEP_STUBS);
-    // List<Map> expected = mock(List.class);
-    // when(parameters.getContext().getContext()).thenReturn("TEST");
-    // when(transaction.select(anyString(), eq(experimentId), eq(true),
-    // eq(rollupDate), anyString()))
-    // .thenReturn(expected);
-    // List<Map> result = databaseAnalytics.getRollupRows(experimentId, rollupDate,
-    // parameters);
-    // assertThat(result, is(expected));
-    // // Exception is thrown in this case
-    // doThrow(new RuntimeException())
-    // .when(transaction)
-    // .select(anyString(), eq(experimentId), eq(true), eq(rollupDate),
-    // anyString());
-    // databaseAnalytics.getRollupRows(experimentId, rollupDate, parameters);
-    // fail();
-    // }
+    @Test(expected = RepositoryException.class)
+    public void getRollupRowsTest() {
+        Experiment.ID experimentId = Experiment.ID.newInstance();
+        String rollupDate = "TEST";
+        Parameters parameters = mock(Parameters.class, RETURNS_DEEP_STUBS);
+        List<Map> expected = mock(List.class);
+        when(parameters.getContext().getContext()).thenReturn("TEST");
+        when(transaction.select(anyString(), eq(experimentId), eq(true), eq(rollupDate), anyString()))
+                .thenReturn(expected);
+        List<Map> result = databaseAnalytics.getRollupRows(experimentId, rollupDate, parameters);
+        assertThat(result, is(expected));
+        // Exception is thrown in this case
+        doThrow(new RuntimeException())
+                .when(transaction)
+                .select(anyString(), eq(experimentId), eq(true), eq(rollupDate), anyString());
+        databaseAnalytics.getRollupRows(experimentId, rollupDate, parameters);
+        fail();
+    }
 
     // @Test(expected = RepositoryException.class)
     @Test
@@ -113,11 +108,8 @@ public class DatabaseAnalyticsTest {
         when(parameters.getActions()).thenReturn(actions);
         List<Map> expected = mock(List.class);
         when(transaction.select(anyString(), any())).thenReturn(expected);
-        assertEquals(parameters.getContext().getContext(), "TEST");
-        // List<Map> result = databaseAnalytics.getActionsRows(experimentId,
-        // parameters);
-        // assertEquals(expected, result);
-        // assertThat(result, is(expected));
+        List<Map> result = databaseAnalytics.getActionsRows(experimentId, parameters);
+        assertThat(result, is(expected));
         // exception while select
         doThrow(new RuntimeException()).when(transaction)
                 .select(anyString(), any());
@@ -125,148 +117,140 @@ public class DatabaseAnalyticsTest {
         fail();
     }
 
-    // @Test(expected = RepositoryException.class)
-    // public void getJointActionsTest() {
-    // Experiment.ID experimentId = Experiment.ID.newInstance();
-    // Parameters parameters = mock(Parameters.class, RETURNS_DEEP_STUBS);
-    // when(parameters.getContext().getContext()).thenReturn("TEST");
-    // Date from = mock(Date.class), to = mock(Date.class);
-    // when(parameters.getFromTime()).thenReturn(from);
-    // when(parameters.getToTime()).thenReturn(to);
-    // List<String> actions = new ArrayList<String>();
-    // actions.add("TEST_ACTION");
-    // when(parameters.getActions()).thenReturn(actions);
-    // List<Map> expected = mock(List.class);
-    // when(transaction.select(anyString(), any())).thenReturn(expected);
-    // List<Map> result = databaseAnalytics.getJointActions(experimentId,
-    // parameters);
-    // assertThat(result, is(expected));
-    // // exception while select
-    // doThrow(new RuntimeException()).when(transaction)
-    // .select(anyString(), any());
-    // databaseAnalytics.getJointActions(experimentId, parameters);
-    // fail();
-    // }
+    @Test(expected = RepositoryException.class)
+    public void getJointActionsTest() {
+        Experiment.ID experimentId = Experiment.ID.newInstance();
+        Parameters parameters = mock(Parameters.class, RETURNS_DEEP_STUBS);
+        when(parameters.getContext().getContext()).thenReturn("TEST");
+        Date from = mock(Date.class), to = mock(Date.class);
+        when(parameters.getFromTime()).thenReturn(from);
+        when(parameters.getToTime()).thenReturn(to);
+        List<String> actions = new ArrayList<String>();
+        actions.add("TEST_ACTION");
+        when(parameters.getActions()).thenReturn(actions);
+        List<Map> expected = mock(List.class);
+        when(transaction.select(anyString(), any())).thenReturn(expected);
+        List<Map> result = databaseAnalytics.getJointActions(experimentId, parameters);
+        assertThat(result, is(expected));
+        // exception while select
+        doThrow(new RuntimeException()).when(transaction)
+                .select(anyString(), any());
+        databaseAnalytics.getJointActions(experimentId, parameters);
+        fail();
+    }
 
-    // @Test(expected = RepositoryException.class)
-    // public void getImpressionRowsTest() {
-    // Experiment.ID experimentId = Experiment.ID.newInstance();
-    // Parameters parameters = mock(Parameters.class, RETURNS_DEEP_STUBS);
-    // when(parameters.getContext().getContext()).thenReturn("TEST");
-    // Date from = mock(Date.class), to = mock(Date.class);
-    // when(parameters.getFromTime()).thenReturn(from);
-    // when(parameters.getToTime()).thenReturn(to);
-    // List<Map> expected = mock(List.class);
-    // when(transaction.select(anyString(), any())).thenReturn(expected);
-    // List<Map> result = databaseAnalytics.getImpressionRows(experimentId,
-    // parameters);
-    // assertThat(result, is(expected));
-    // // exception while select
-    // doThrow(new RuntimeException()).when(transaction)
-    // .select(anyString(), any());
-    // databaseAnalytics.getImpressionRows(experimentId, parameters);
-    // fail();
-    // }
+    @Test(expected = RepositoryException.class)
+    public void getImpressionRowsTest() {
+        Experiment.ID experimentId = Experiment.ID.newInstance();
+        Parameters parameters = mock(Parameters.class, RETURNS_DEEP_STUBS);
+        when(parameters.getContext().getContext()).thenReturn("TEST");
+        Date from = mock(Date.class), to = mock(Date.class);
+        when(parameters.getFromTime()).thenReturn(from);
+        when(parameters.getToTime()).thenReturn(to);
+        List<Map> expected = mock(List.class);
+        when(transaction.select(anyString(), any())).thenReturn(expected);
+        List<Map> result = databaseAnalytics.getImpressionRows(experimentId, parameters);
+        assertThat(result, is(expected));
+        // exception while select
+        doThrow(new RuntimeException()).when(transaction)
+                .select(anyString(), any());
+        databaseAnalytics.getImpressionRows(experimentId, parameters);
+        fail();
+    }
 
-    // @Test(expected = RepositoryException.class)
-    // public void getEmptyBucketsTest() {
-    // Experiment.ID experimentId = Experiment.ID.newInstance();
-    // List<Map> input = new ArrayList<Map>();
-    // Map<String, String> map = new HashMap<String, String>();
-    // map.put("label", "TEST_LABEL");
-    // input.add(map);
-    // when(transaction.select(anyString(), any())).thenReturn(input);
-    // Map<Bucket.Label, BucketCounts> result =
-    // databaseAnalytics.getEmptyBuckets(experimentId);
-    // assertThat(result.size(), is(1));
-    // assertThat(result.get(Bucket.Label.valueOf("TEST_LABEL")).getLabel().toString(),
-    // is("TEST_LABEL"));
-    // assertThat(result.get(Bucket.Label.valueOf("TEST_LABEL")).getActionCounts().size(),
-    // is(0));
-    // // exception while select
-    // doThrow(new RuntimeException()).when(transaction)
-    // .select(anyString(), any());
-    // databaseAnalytics.getEmptyBuckets(experimentId);
-    // fail();
-    // }
+    @Test(expected = RepositoryException.class)
+    public void getEmptyBucketsTest() {
+        Experiment.ID experimentId = Experiment.ID.newInstance();
+        List<Map> input = new ArrayList<Map>();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("label", "TEST_LABEL");
+        input.add(map);
+        when(transaction.select(anyString(), any())).thenReturn(input);
+        Map<Bucket.Label, BucketCounts> result = databaseAnalytics.getEmptyBuckets(experimentId);
+        assertThat(result.size(), is(1));
+        assertThat(result.get(Bucket.Label.valueOf("TEST_LABEL")).getLabel().toString(), is("TEST_LABEL"));
+        assertThat(result.get(Bucket.Label.valueOf("TEST_LABEL")).getActionCounts().size(), is(0));
+        // exception while select
+        doThrow(new RuntimeException()).when(transaction)
+                .select(anyString(), any());
+        databaseAnalytics.getEmptyBuckets(experimentId);
+        fail();
+    }
 
-    // @Test(expected = RepositoryException.class)
-    // public void getCountsFromRollupsTest() {
-    // List<Map> expected = mock(List.class);
-    // Experiment.ID experimentId = Experiment.ID.newInstance();
-    // Parameters parameters = mock(Parameters.class, RETURNS_DEEP_STUBS);
-    // when(parameters.getContext().getContext()).thenReturn("TEST");
-    // when(transaction.select(anyString(), any())).thenReturn(expected);
-    // List<Map> result = databaseAnalytics.getCountsFromRollups(experimentId,
-    // parameters);
-    // assertThat(result, is(expected));
-    // // exception while select
-    // doThrow(new RuntimeException()).when(transaction)
-    // .select(anyString(), any());
-    // databaseAnalytics.getCountsFromRollups(experimentId, parameters);
-    // fail();
-    // }
+    @Test(expected = RepositoryException.class)
+    public void getCountsFromRollupsTest() {
+        List<Map> expected = mock(List.class);
+        Experiment.ID experimentId = Experiment.ID.newInstance();
+        Parameters parameters = mock(Parameters.class, RETURNS_DEEP_STUBS);
+        when(parameters.getContext().getContext()).thenReturn("TEST");
+        when(transaction.select(anyString(), any())).thenReturn(expected);
+        List<Map> result = databaseAnalytics.getCountsFromRollups(experimentId, parameters);
+        assertThat(result, is(expected));
+        // exception while select
+        doThrow(new RuntimeException()).when(transaction)
+                .select(anyString(), any());
+        databaseAnalytics.getCountsFromRollups(experimentId, parameters);
+        fail();
+    }
 
-    // @Test(expected = RepositoryException.class)
-    // public void checkMostRecentRollupTest() {
-    // Experiment.ID experimentId = Experiment.ID.newInstance();
-    // Experiment experiment = mock(Experiment.class);
-    // when(experiment.getID()).thenReturn(experimentId);
-    // List queryResult = mock(List.class);
-    // Parameters parameters = mock(Parameters.class, RETURNS_DEEP_STUBS);
-    // when(parameters.getContext().getContext()).thenReturn("TEST");
-    // Date to = mock(Date.class);
-    // when(to.getTime()).thenReturn(9999L);
-    // when(transaction.select(anyString(), eq(experimentId),
-    // eq("TEST"))).thenReturn(queryResult);
-    // when(queryResult.isEmpty()).thenReturn(true);
-    // boolean result = databaseAnalytics.checkMostRecentRollup(experiment,
-    // parameters, to);
-    // assertThat(result, is(true));
+    @Test(expected = RepositoryException.class)
+    public void checkMostRecentRollupTest() {
+        Experiment.ID experimentId = Experiment.ID.newInstance();
+        Experiment experiment = mock(Experiment.class);
+        when(experiment.getID()).thenReturn(experimentId);
+        List queryResult = mock(List.class);
+        Parameters parameters = mock(Parameters.class, RETURNS_DEEP_STUBS);
+        when(parameters.getContext().getContext()).thenReturn("TEST");
+        Date to = mock(Date.class);
+        when(to.getTime()).thenReturn(9999L);
+        when(transaction.select(anyString(), eq(experimentId), eq("TEST"))).thenReturn(queryResult);
+        when(queryResult.isEmpty()).thenReturn(true);
+        boolean result = databaseAnalytics.checkMostRecentRollup(experiment, parameters, to);
+        assertThat(result, is(true));
 
-    // when(queryResult.isEmpty()).thenReturn(false);
-    // Map map = mock(Map.class);
-    // Date maxDay = mock(Date.class);
-    // when(queryResult.get(0)).thenReturn(map);
-    // when(map.get("day")).thenReturn(maxDay);
-    // when(maxDay.getTime()).thenReturn(100000L);
-    // result = databaseAnalytics.checkMostRecentRollup(experiment, parameters, to);
-    // assertThat(result, is(true));
+        when(queryResult.isEmpty()).thenReturn(false);
+        Map map = mock(Map.class);
+        Date maxDay = mock(Date.class);
+        when(queryResult.get(0)).thenReturn(map);
+        when(map.get("day")).thenReturn(maxDay);
+        when(maxDay.getTime()).thenReturn(100000L);
+        result = databaseAnalytics.checkMostRecentRollup(experiment, parameters, to);
+        assertThat(result, is(true));
 
-    // when(maxDay.getTime()).thenReturn(1000L);
-    // result = databaseAnalytics.checkMostRecentRollup(experiment, parameters, to);
-    // assertThat(result, is(false));
+        when(maxDay.getTime()).thenReturn(1000L);
+        result = databaseAnalytics.checkMostRecentRollup(experiment, parameters, to);
+        assertThat(result, is(false));
 
-    // doThrow(new RuntimeException()).when(transaction)
-    // .select(anyString(), eq(experimentId), eq("TEST"));
-    // databaseAnalytics.checkMostRecentRollup(experiment, parameters, to);
-    // fail();
+        doThrow(new RuntimeException()).when(transaction)
+                .select(anyString(), eq(experimentId), eq("TEST"));
+        databaseAnalytics.checkMostRecentRollup(experiment, parameters, to);
+        fail();
 
-    // }
+    }
 
-    // @Test
-    // public void testAddActionsToSql() {
-    // Parameters parameters = mock(Parameters.class);
-    // when(parameters.getActions()).thenReturn(null);
-    // StringBuilder stringBuilder = new StringBuilder();
-    // List param = new ArrayList<>();
-    // databaseAnalytics.addActionsToSql(parameters, stringBuilder, param);
-    // assertThat(stringBuilder.toString(), is(""));
-    // assertThat(param.size(), is(0));
-    // List<String> actions = new ArrayList<>();
-    // actions.add("a1");
-    // when(parameters.getActions()).thenReturn(actions);
-    // databaseAnalytics.addActionsToSql(parameters, stringBuilder, param);
-    // assertThat(actions.size(), is(1));
-    // assertThat(param.size(), is(1));
-    // assertThat(stringBuilder.toString(), is(" and action in (?) "));
-    // stringBuilder.setLength(0); // reset the builder
-    // param.clear();
-    // actions.add("a2");
-    // databaseAnalytics.addActionsToSql(parameters, stringBuilder, param);
-    // assertThat(actions.size(), is(2));
-    // assertThat(param.size(), is(2));
-    // assertThat(stringBuilder.toString(), is(" and action in (?,?) "));
-    // }
+    @Test
+    public void testAddActionsToSql() {
+        Parameters parameters = mock(Parameters.class);
+        when(parameters.getActions()).thenReturn(null);
+        StringBuilder stringBuilder = new StringBuilder();
+        List param = new ArrayList<>();
+        databaseAnalytics.addActionsToSql(parameters, stringBuilder, param);
+        assertThat(stringBuilder.toString(), is(""));
+        assertThat(param.size(), is(0));
+        List<String> actions = new ArrayList<>();
+        actions.add("a1");
+        when(parameters.getActions()).thenReturn(actions);
+        databaseAnalytics.addActionsToSql(parameters, stringBuilder, param);
+        assertThat(actions.size(), is(1));
+        assertThat(param.size(), is(1));
+        assertThat(stringBuilder.toString(), is(" and action in (?) "));
+        stringBuilder.setLength(0); // reset the builder
+        param.clear();
+        actions.add("a2");
+        databaseAnalytics.addActionsToSql(parameters, stringBuilder, param);
+        assertThat(actions.size(), is(2));
+        assertThat(param.size(), is(2));
+        assertThat(stringBuilder.toString(), is(" and action in (?,?) "));
+    }
 
 }
